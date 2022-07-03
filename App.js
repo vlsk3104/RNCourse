fca354df5e698a38b059e16ad1274ae5cacf39bf
vlsk3104 <vlsk3104@google.com>
2022-07-03 14:39:47 +0900

    IOS & Android Styling Differences

--- App.js
+++ App.js
@@ -27,7 +27,11 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal, index) => <Text key={index}>{goal}</Text>)}
+        {courseGoals.map((goal, index) => (
+          <View key={index} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -57,5 +61,14 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
-  }
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
+  },
 });
