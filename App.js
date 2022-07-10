017ef728628c226790438f35690d1fb6618d8677
vlsk3104 <vlsk3104@google.com>
2022-07-10 14:40:10 +0900

    Opening & Closing the Modal

--- App.js
+++ App.js
@@ -11,11 +11,16 @@ export default function App() {
     setModalIsVisible(true);
   }
 
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: enteredGoalText, id: Math.random().toString() },
     ]);
+    endAddGoalHandler();
   }
 
   function deleteGoalHandler(id) {
@@ -34,6 +39,7 @@ export default function App() {
       <GoalInput
         visible={modalIsVisible}
         onAddGoal={addGoalHandler}
+        onCancel={endAddGoalHandler}
       />
       <View style={styles.goalsContainer}>
         <FlatList
